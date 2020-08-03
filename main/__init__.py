f2d5f8688f94a719fad07cf691dd943c12e4f8b8
jod35 <jodestrevin@gmail.com>
2020-08-03 12:50:41 -0700

    add(book model and relationship with author model)

--- main/__init__.py
+++ main/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .config import DevConfig
 from main.utils.database import db
 from flask import jsonify,make_response
+from flask_migrate import Migrate
 
 
 
@@ -9,6 +10,8 @@ app=Flask(__name__)
 
 app.config.from_object(DevConfig)
 db.init_app(app)
+
+migrate=Migrate(app,db)
 
 from main.api.views import api_blueprint
 app.register_blueprint(api_blueprint,url_prefix='/api')
@@ -41,12 +44,14 @@ def method_not_allowed(err):
 
 
 from main.models.authors import Author
+from main.models.books import Book
 @app.shell_context_processor
 def make_shell_context():
     return {
         'app':app,
         'db':db,
-        'Author':Author
+        'Author':Author,
+        'Book':Book
     }
 
 
